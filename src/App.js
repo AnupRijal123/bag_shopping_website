22cc1037557b4a0c007ded93095098645d2eb283
Anup <anuprijal321@gmail.com>
2025-05-16 23:17:37 +0545

    Completed upto follow us section css

--- src/App.js
+++ src/App.js
@@ -13,6 +13,21 @@ function App() {
         <CardSection />
       </div>
 
+      <div className="section-container coloured-background-section">
+
+        <div className="coloured-background-section-image-container">
+          <img className="coloured-background-section-image" src={require('./assets/images/bannerimage.jpg')} alt="model-image" />
+        </div>
+
+        <div className="coloured-background-content">
+          <h1>Follow us for updates and exclusive offers on</h1>
+          <div className="social-media-icon-container">
+            <p>f</p>
+            <p>i</p>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -6,13 +6,121 @@ function CardSection() {
             <h1 className="card-heading-text">card heading</h1>
 
             <div className="card-container">
-                <div className="card">hello</div>
-                <div className="card">hello</div>
-                <div className="card">hello</div>
-                <div className="card">hello</div>
-                <div className="card">hello</div>
-                <div className="card">hello</div>
-                <div className="card">hello</div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+                <div className="card">
+                    <div className="tag">
+                        <p className="tag-text small-text">Out Of Stock</p>
+                    </div>
+                    <img className="card-image" src={require('../assets/images/bannerimage.jpg')} alt="card-item" />
+                    <h2 className="card-item-name-text">Nike Bag</h2>
+
+                    <div className="card-price-container">
+                        <p className="strike-text">Rs 1500</p>
+                        <p>Rs 800</p>
+                    </div>
+
+                    <p className="read-more-text">Read more</p>
+                </div>
+
+
 
             </div>
         </>
