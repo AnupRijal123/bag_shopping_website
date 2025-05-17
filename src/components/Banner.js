9ad9eb9b2f1de2a5124e6c0fe3677e26124caa07
Anup <anuprijal321@gmail.com>
2025-05-17 13:08:09 +0545

    Added video in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,10 @@ import '../styles/Banner.css';
 function Banner() {
     return (
         <div className="banner-container">
-            <img className="banner-image" src={require('../assets/images/bannerimage.jpg')} alt="banner" />
+            {/* <img className="banner-image" src={require('../assets/images/bannerimage.jpg')} alt="banner" /> */}
+            <video className="banner-video" autoPlay muted loop>
+                <source src={require('../assets/videos/video1.mp4')} type="video/mp4" />
+            </video>
             <div className="fade-container"></div>
             <div className="banner-content">hello</div>
         </div>
