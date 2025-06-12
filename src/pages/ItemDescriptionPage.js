238dc52ac1fbb9a63f010b5a517ab4b16408c60e
Anup <anuprijal321@gmail.com>
2025-06-12 17:25:20 +0545

    Doing item description page

--- src/pages/ItemDescriptionPage.js
+++ src/pages/ItemDescriptionPage.js
@@ -1,6 +1,41 @@
+import '../styles/ItemDescriptionPage.css';
+
 function ItemDescriptionPage() {
     return (
-        <h1>item description</h1>
+        <div className="item-description-container">
+            <div className="top-container"></div>
+            <h1 className="item-description-heading-text">Nike Bag aaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasssssssssssssssssssssssssssssssssssss</h1>
+
+            <div className="item-row">
+                <div className="item-image-container">1</div>
+                <div className="item-content-container">
+
+
+
+                    <div className="button-container">
+
+                        <div className="button-layout button-transparent-background button-gray-border">
+                            <div className="button-background-container button-gray-background"></div>
+                            <p className="button-text dark-gray-text">hello</p>
+                        </div>
+
+                        <div className="button-layout button-transparent-background button-gray-border">
+                            <div className="button-background-container button-gray-background"></div>
+                            <p className="button-text dark-gray-text">hello</p>
+                        </div>
+
+
+                    </div>
+
+                    <p>this is description</p>
+
+
+
+                </div>
+
+
+            </div>
+        </div>
     )
 }
 
