8874de07a0f964663cc320ee80962b2798716df0
Anup <anuprijal321@gmail.com>
2025-07-21 11:53:53 +0545

    Added table width inline css

--- src/components/AdminViewOrders.js
+++ src/components/AdminViewOrders.js
@@ -9,70 +9,86 @@ function AdminViewOrders() {
 
     useEffect(() => {
         console.log('admin order page mounted');
-        async function getOrders() {
-            const { data, error } = await supabase
-                .from("orders_information")
-                .select("*");
 
-            if (data) {
-                setOrdersArray(data)
-            }
-            if (error) {
-                console.error("Error fetching data", error);
-            }
-        }
 
         getOrders();
 
     }, []);
 
+    async function getOrders() {
+        const { data, error } = await supabase
+            .from("orders_information")
+            .select("*");
+
+        if (data) {
+            setOrdersArray(data);
+        }
+        if (error) {
+            console.error("Error fetching data", error);
+        }
+    }
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>SN</th>
-                    <th>Order Number</th>
-                    <th>Full Name</th>
-                    <th>Contact</th>
-                    <th>Delivery Address</th>
-                    <th>Order Details</th>
-                    <th>Order Placed Date</th>
-                    <th>Total Price</th>
-                    <th>Delivery Date</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
+        <>
+            <button onClick={getOrders}>Refresh Table Data</button>
+            <table>
+                <colgroup>
+                    <col style={{ width: '10px' }} />
+                    <col style={{ width: '120px' }} />
+                    <col />
+                    <col />
+                    <col style={{ width: '140px' }} />
 
-            <tbody>
-                {ordersArray.map((item, index) => (
-                    <tr key={item.id}>
-                        <td>{index + 1}</td>
-                        <td>{item.order_no}</td>
-                        <td>{item.full_name}</td>
-                        <td>{item.contact_no}</td>
-                        <td>{item.delivery_address}</td>
-                        <td>
-                            {item.order_details.map((i, index) => (
-                                <div key={index}>
-                                    <p>Code : {i.code}</p>
-                                    <p>Colour :{i.colour}</p>
-                                    <p>Price : {i.price}</p>
-                                    <img src={i.img} className="order-item-image" alt="order-image" />
-                                </div>
 
-                            ))}
-                        </td>
 
-                        <td>{new Date(item.created_at).toLocaleDateString('en-CA')}</td>
-                        <td>{item.total_price}</td>
-                        <td>{item.delivery_date}</td>
-                        <td>{item.order_status}</td>
-
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>SN</th>
+                        <th>Order Number</th>
+                        <th>Full Name</th>
+                        <th>Contact</th>
+                        <th>Delivery Address</th>
+                        <th>Order Details</th>
+                        <th>Order Placed Date</th>
+                        <th>Total Price</th>
+                        <th>Delivery Date</th>
+                        <th>Status</th>
                     </tr>
-                ))}
+                </thead>
 
-            </tbody>
-        </table>
+                <tbody>
+                    {ordersArray.map((item, index) => (
+                        <tr key={item.id}>
+                            <td>{index + 1}</td>
+                            <td>{item.order_no}</td>
+                            <td>{item.full_name}</td>
+                            <td>{item.contact_no}</td>
+                            <td>{item.delivery_address}</td>
+                            <td>
+                                {item.order_details.map((i, index) => (
+                                    <div key={index}>
+                                        <p>Code : {i.code}</p>
+                                        <p>Colour :{i.colour}</p>
+                                        <p>Price : {i.price}</p>
+                                        <img src={i.img} className="order-item-image" alt="order-image" />
+                                    </div>
+
+                                ))}
+                            </td>
+
+                            <td>{new Date(item.created_at).toLocaleDateString('en-CA')}</td>
+                            <td>{item.total_price}</td>
+                            <td>{item.delivery_date}</td>
+                            <td>{item.order_status}</td>
+
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+
+        </>
     )
 }
 
